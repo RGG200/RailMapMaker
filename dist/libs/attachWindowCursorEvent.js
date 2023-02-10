768e9f0a6cfe8e4652c68a263902425284ffb5e9
RGG200 <75140347+RGG200@users.noreply.github.com>
2023-02-10 17:49:28 +0100

    Update attachWindowCursorEvent.js

--- dist/libs/attachWindowCursorEvent.js
+++ dist/libs/attachWindowCursorEvent.js
@@ -76,7 +76,7 @@ const attachEvent = {
                         "type": "withSecondaryName",
                         "name": [
                             ["Val de Fontenay", "Vincennes", "Nation", "Gare de Lyon", "Auber", "Châtelet Les Halles", "La défense"][randomSeedA] + ["Est", "Ouest", "Nord", "Sud", "Centre"][randomSeedB],
-                                ["Les alouettes", "La place", "Centre administratif", "Centre commercial", "La grande Arche"][randomSeedA] + [" East", " West", " South", " North", " Middle"][randomSeedB]
+                            ["Les alouettes", "La place", "Centre administratif", "Centre commercial", "La grande Arche"][randomSeedA] + [" East", " West", " South", " North", " Middle"][randomSeedB]
                         ],
                         "position": 1,
                         "alignment": "middle",
