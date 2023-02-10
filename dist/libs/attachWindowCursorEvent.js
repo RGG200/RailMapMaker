b217fc4723f1433b20c139bc18edc5266fdbdbb4
RGG200 <75140347+RGG200@users.noreply.github.com>
2023-02-10 18:22:58 +0100

    Update attachWindowCursorEvent.js

--- dist/libs/attachWindowCursorEvent.js
+++ dist/libs/attachWindowCursorEvent.js
@@ -53,8 +53,8 @@ const attachEvent = {
                         "text": {
                             "type": "withSecondaryName",
                             "name": [
-                                ["Val de Fontenay", "Vincennes", "Nation", "Gare de Lyon", "Auber", "Châtelet Les Halles", "La défense"][randomSeedA] + ["-Est", "-Ouest", "-Nord", "-Sud", "-Centre"][randomSeedA],
-                                ["The alouettes", "The woods", " Administrative Center", "Shop", "La grande Arche"][randomSeedA] + ["-East", "-West", "-South", "-North", "-Middle"][randomSeedB]
+                                ["Fontenay", "Vincennes", "Nation", "Gare de Lyon", "Auber", "Châtelet Les Halles", "La défense"][randomSeedA] + ["-Est", "-Ouest", "-Nord", "-Sud", "-Centre"][randomSeedA],
+                      ["Sous-Bois", "Sur-Seine", "Porte", "Mairie", "La-Grande-Arche"][randomSeedA] + ["-Est", "-Ouest", "-Nord", "-Sud", "-Centre"][randomSeedB]
                             ],
                             "position": 1,
                             "alignment": "middle"
@@ -75,8 +75,8 @@ const attachEvent = {
                     "text": {
                         "type": "withSecondaryName",
                         "name": [
-                                ["Val de Fontenay", "Vincennes", "Nation", "Gare de Lyon", "Auber", "Châtelet Les Halles", "La défense"][randomSeedA] + ["-Est", "-Ouest", "-Nord", "-Sud", "-Centre"][randomSeedB],
-                                ["The alouettes", "The woods", " Administrative Center", "Shop", "La grande Arche"][randomSeedA] + ["-East", "-West", "-South", "-North", "-Middle"][randomSeedB]
+                                ["Fontenay", "Vincennes", "Nation", "Gare de Lyon", "Auber", "Châtelet Les Halles", "La défense"][randomSeedA] + ["-Est", "-Ouest", "-Nord", "-Sud", "-Centre"][randomSeedA],
+                      ["Sous-Bois", "Sur-Seine", "Porte", "Mairie", "La-Grande-Arche"][randomSeedA] + ["-Est", "-Ouest", "-Nord", "-Sud", "-Centre"][randomSeedB]
                         ],
                         "position": 1,
                         "alignment": "middle",
