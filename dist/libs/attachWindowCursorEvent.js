addc7f3c3f60a2e0fd7d85a338956f10a587e1e9
RGG200 <75140347+RGG200@users.noreply.github.com>
2023-02-10 18:32:12 +0100

    Update attachWindowCursorEvent.js

--- dist/libs/attachWindowCursorEvent.js
+++ dist/libs/attachWindowCursorEvent.js
@@ -53,8 +53,8 @@ const attachEvent = {
                         "text": {
                             "type": "withSecondaryName",
                             "name": [
-                                ["Fontenay", "Vincennes", "Nation", "Gare de Lyon", "Auber", "Châtelet Les Halles", "La défense"][randomSeedA] + ["-Est", "-Ouest", "-Nord", "-Sud", "-Centre"][randomSeedA],
-                                ["Sous-Bois", "Sur-Seine", "Porte", "Mairie", "La-Grande-Arche"][randomSeedA] + ["-Est", "-Ouest", "-Nord", "-Sud", "-Centre"][randomSeedB]
+                                ["Fontenay", "Vincennes", "Nation", "Gare de Lyon", "Auber", "Châtelet Les Halles", "La défense"][randomSeedA] + ["-Est", "-Ouest", "-Nord", "-Sud", "-Centre"][randomSeedB],
+                                ["Sous-Bois", "Sur-Seine", "Porte", "Mairie", "La-Grande-Arche"][randomSeedA]
                             ],
                             "position": 1,
                             "alignment": "middle"
@@ -75,8 +75,8 @@ const attachEvent = {
                     "text": {
                         "type": "withSecondaryName",
                         "name": [
-                                ["Fontenay", "Vincennes", "Nation", "Gare de Lyon", "Auber", "Châtelet Les Halles", "La défense"][randomSeedA] + ["-Est", "-Ouest", "-Nord", "-Sud", "-Centre"][randomSeedA],
-                                ["Sous-Bois", "Sur-Seine", "Porte", "Mairie", "La-Grande-Arche"][randomSeedA] + ["-Est", "-Ouest", "-Nord", "-Sud", "-Centre"][randomSeedB]
+                                ["Fontenay", "Vincennes", "Nation", "Gare de Lyon", "Auber", "Châtelet Les Halles", "La défense"][randomSeedA] + ["-Est", "-Ouest", "-Nord", "-Sud", "-Centre"][randomSeedB],
+                                ["Sous-Bois", "Sur-Seine", "Porte", "Mairie", "La-Grande-Arche"][randomSeedA]
                         ],
                         "position": 1,
                         "alignment": "middle",
